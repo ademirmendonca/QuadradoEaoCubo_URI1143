d878bf183a3b60fd95cf2b00f1748142eabb2d3e
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-06-23 18:04:58 -0300

    Add project files.

---
+++ QuadradoEaoCubo_URI1143/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace QuadradoEaoCubo_URI1143
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                double totalQuadrado =  Math.Pow(i, + 2);
+                double totalCubo = Math.Pow(i, + 3);
+                Console.WriteLine(i + " " + totalQuadrado + " " + totalCubo);                
+            }
+        }
+    }
+}
